b2821d88c51ee57def414da95d23146dea36f09b
lohamedevelop <lohamedevelop@gmail.com>
2022-09-26 23:57:50 -0300

    migrations

---
+++ database/migrations/2022_09_27_025312_create_climas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('climas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('cidade_id');
+            $table->string('temperatura');
+            $table->string('sensacao_termica');
+            $table->string('hora');
+            $table->timestamps();
+            $table->foreign('cidade_id')->references('id')->on('cidades');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('climas');
+    }
+};
